e2b798f024720af3bde3556ea7a0096d6e1ea293
Matías Otermín <mati_otermin@yahoo.com>
2020-08-11 23:48:59 -0300

    Punto 3, Matías

--- mafia.pl
+++ mafia.pl
@@ -10,10 +10,7 @@ rol(hibbert, medico).
 rol(lisa, detective).
 rol(rafa, detective).
 
-
-% RONDAS
-
-% Ejercicio 1:
+% Ejercicio 1: rondas
 
 % a).
 
@@ -27,7 +24,6 @@ investiga(investigador, investigado).
 salva(medico, salvado).
 eliminado(eliminado).
 */
-
 
 ronda(1, atacado(lisa)).
 ronda(1, salva(nick, nick)).
@@ -95,15 +91,14 @@ test(perdio_es_completamente_inversible, set((Persona, Ronda) = [(nick, 1) ,(raf
 
 :- end_tests(perdedores).
 
-% GANADOR 
 
-% Ejercicio 2. 
 
-/* 
-Conocer los contrincantes de una persona, o sea, los del otro bando. 
-Si la persona pertenece a la mafia, los contrincantes son todos aquellos 
-que no forman parte de la mafia; y viceversa. Esta relación debe ser simétrica.
- */
+% Ejercicio 2: contrincantes y ganador.
+
+% Integrante 1: Mercedes
+
+% a).
+
 contrincante(Jugador, Contrincante):- sonContrincantes(Jugador, Contrincante).
 contrincante(Jugador, Contrincante):- sonContrincantes(Contrincante, Jugador).
 
@@ -112,10 +107,12 @@ sonContrincantes(Jugador, Contrincante):-
     rol(Contrincante, RolContrincante),
     RolContrincante \= mafia. 
 
+% b).
+
 gano(Jugador):-
     esJugador(Jugador),
-    not(perdio(Jugador, _)), %no existe al menos una ronda en la que haya perdido (involucion not)
-    %not(perdio(Jugador, _)) puede ser hecho con un forall pero es mas sencillo x la involucion
+    not(perdio(Jugador, _)), % No existe al menos una ronda en la que haya perdido (involución del not)
+    %not(perdio(Jugador, _)) puede ser hecho con un forall pero es mas sencillo por la involución
     forall(contrincante(Jugador, Contrincante), perdio(Contrincante, _)).
 
 esJugador(Jugador):-
@@ -125,41 +122,120 @@ esJugador(Jugador):-
  preguntar lo de la inversibilidad
 */
 
+% c). Casos de prueba
+
 :- begin_tests(contrincantes).
+
+% Consultas individuales:
 
 test(un_miembro_de_la_mafia_no_es_contrincante_de_otro_miembro_de_la_mafia, fail) :- contrincante(tony, maggie).
 
 test(un_miembro_de_la_mafia_es_contrincante_de_un_jugador_que_no_es_miembro_de_la_mafia, nondet) :- contrincante(homero, bart).
 
+% Consulta existencial (prueba de inversibilidad):
+
 %falta probar inversibilidad -> muy largo 
 %test(contrincantes_es_inversible, ) :- .
 :- end_tests(contrincantes).
 
-:-begin_tests(gano).
+:-begin_tests(ganadores).
+
+% Consultas individuales:
 
 test(un_jugador_gana_cuando_no_pierde_ninguna_ronda_y_todos_sus_contrincantes_si, nondet) :- gano(maggie).
 
 test(un_jugador_no_gana_cuando_pierde_al_menos_una_ronda, fail) :- gano(nick).
 
 % rari: test(un_jugador_no_gana_cuando_no_pierde_ninguna_ronda_pero_sus_contrincantes_no_pierden, fail) :-gano().
+
+% Consulta existencial (prueba de inversibilidad):
+
 test(gano_es_inversible, set(Jugador = [maggie])) :- gano(Jugador).
 
-end_tests(gano).
-
-/* 
-:- begin_tests(nombre).
-
-test(nombre, nondet) :- consulta.
-
-test(nombre, fail) :- consulta.
-
-test(nombre_es_totalmente_inversible, set((Jugador, Continente) = [(amarillo, americaDelNorte), (negro, oceania)])) :- ocupaContinente(Jugador, Continente).
-
-:- end_tests(nombre).
-
- */
+end_tests(ganadores).
 
 
+% Ejercicio 3: imbatibles.
 
+% Integrante 2: Matías
+
+% a).
+
+imbatible(Medico):-
+    medico(Medico),
+    forall(rondaAnteriorOIgualEnLaQueFueEliminado(Ronda, Medico), fueImbatible(Medico, Ronda)).
+
+imbatible(Detective):-
+    detective(Detective),
+    forall(rol(Persona, mafia), investigo(Detective, Persona)).
+
+rondaAnteriorOIgualEnLaQueFueEliminado(Ronda, Medico):-
+    ronda(RondaEliminado, eliminado(Medico)),
+    ronda(Ronda, _),
+    Ronda =< RondaEliminado.
+    
+fueImbatible(Medico, Ronda):-
+    ronda(Ronda, _),
+    forall(personaAtacadaNoSalvadaPorOtroMedico(Persona, Medico, Ronda), salvo(Medico, Persona, Ronda)).
+
+personaAtacadaNoSalvadaPorOtroMedico(Persona, Medico, Ronda):-
+    ronda(Ronda, atacado(Persona)),
+    medico(OtroMedico),
+    OtroMedico \= Medico, % No habrá problemas de inversibilidad, pues la variable Medico ya viene ligada y el predicado sólo se usa en fueImbatible/2.
+    not(ronda(Ronda, salvo(OtroMedico, Persona))).
+
+medico(Persona):-
+    rol(Persona, medico).
+
+detective(Persona):-
+    rol(Persona, detective).
+
+salvo(Medico, Persona, Ronda):-
+    ronda(Ronda, salva(Medico, Persona)).
+
+investigo(Detective, Persona):-
+    ronda(_, investiga(Detective, Persona)).
+
+/*
+El concepto de inversibilidad está presente en ciertos predicados y en otros no (especialmente el concepto de predicado completamente inversible). Esto
+se da por ejemplo, con los predicados imbatible/1 y personaAtacadaNoSalvadaPorOtroMedico/3 respectivamente. Para el caso de los predicados no 
+completamente inversibles (como el último mencionado), muchas veces no es necesario que posean dicha característica, ya que como se explicó en la línea
+174, es un predicado que se utiliza como condición de una regla en la que las variables ya están ligadas al "ingresar" al mismo. En cambio, el predicado
+imbatible/1 sí es completamente inversible (obviamente que esperamos de él esta característica).
+
+Para el caso del resto de las personas que no son imbatibles, se tiene en cuenta el principio de universo cerrado, tomando así como falso a todo aquello
+que no se pueda inferir como verdadero a partir la base de conocimientos. Relacionando este concepto con el de inversibilidad, se puede observar que por
+ejemplo no se podría saber cuáles son aquellas personas que no son imbatibles a través de la consulta not(imbatible(Persona)). ya que la misma retornaría
+false. debido a que el predicado not/1 no es inversible. De hecho, al no figurar en la base de conocimientos aquellas personas que no son imbatibles, por
+el principio de universo cerrado, las mismas son infinitas (en realidad cualquier átomo o número por ejemplo que no sea un individuo perteneciente a
+la base de conocimientos no será imbatible), salvo que se cree un predicado específico para ello acotando el universo de las personas y así poder obtener
+aquellas que no son imbatibles. El mismo podría desarrollarse de la siguiente manera:
+
+noImbatible(Persona):-
+    esJugador(Persona),
+    not(imbatible(Persona)).
+*/
+
+% b). Casos de prueba
+
+:-begin_tests(personas_imbatibles).
+
+% Consultas individuales:
+
+test(un_medico_que_siempre_salvo_a_alguien_que_estaba_siendo_atacado_por_la_mafia_es_imbatible, nondet) :- imbatible(hibbert).
+
+test(un_medico_que_no_siempre_salvo_a_alguien_que_estaba_siendo_atacado_por_la_mafia_no_es_imbatible, fail) :- imbatible(nick).
+
+test(un_detective_que_investigo_a_todas_las_personas_que_pertenecen_a_la_mafia_es_imbatible, nondet) :- imbatible(lisa).
+
+test(un_detective_que_no_investigo_a_todas_las_personas_que_pertenecen_a_la_mafia_no_es_imbatible, fail) :- imbatible(rafa).
+
+test(un_civil_nunca_es_imbatible, fail) :- imbatible(homero).
+
+% Consulta existencial (prueba de inversibilidad):
+
+test(quienes_son_imbatibles, set(Persona = [hibbert, lisa])) :- imbatible(Persona).
+
+end_tests(personas_imbatibles).
 
 
